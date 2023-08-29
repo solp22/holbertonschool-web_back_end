adc6dfc8f6449bb894768f68f36c868623c12408
solp22 <sol.puentereal@gmail.com>
2023-08-29 14:23:31 -0700

    pycode

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -2,6 +2,7 @@
 """element_length module"""
 from typing import List, Tuple, Iterable, Sequence
 
+
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """element lenght func"""
     return [(i, len(i)) for i in lst]
