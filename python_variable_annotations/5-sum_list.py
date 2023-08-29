ff6bafa6ac16003025b1547ebd1f4f08a6a37c8e
solp22 <sol.puentereal@gmail.com>
2023-08-29 13:33:16 -0700

    5-sum_list

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""sum_list module"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """return sum of elem in list"""
+    return sum(input_list)
