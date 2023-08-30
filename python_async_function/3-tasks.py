78333c4df9d240b2e332ab907c54dc4b2e85dba0
solp22 <sol.puentereal@gmail.com>
2023-08-30 15:37:13 -0700

    pycode

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """tasks module"""
-wait_random = __import__('0-basic_async_syntax').wait_random
 import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
