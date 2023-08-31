9baf5e4e494cfdb2e888f18948e7dc2561a4a950
solp22 <sol.puentereal@gmail.com>
2023-08-31 10:40:12 -0700

    make it a list comprehension

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -4,9 +4,6 @@ from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() ->  List[float]:
+async def async_comprehension() -> List[float]:
     """collect 10 numbers from generator"""
-    generator_list = []
-    async for item in async_generator():
-        generator_list.append(item)
-    return generator_list
+    return [item async for item in async_generator()] 
