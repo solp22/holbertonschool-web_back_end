67623ee671cb7bbebb01526ed31f6162cd88c101
solp22 <sol.puentereal@gmail.com>
2023-08-31 10:41:18 -0700

    pycode

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -6,4 +6,4 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     """collect 10 numbers from generator"""
-    return [item async for item in async_generator()] 
+    return [item async for item in async_generator()]
