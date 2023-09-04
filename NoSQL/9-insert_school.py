850dea03777ecbf70332b6312cb88f03a1548bf3
solp22 <sol.puentereal@gmail.com>
2023-09-04 15:32:10 -0700

    task 9

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""insert_school module"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """ inserts a new document in a collection"""
+    return mongo_collection.insert_one(kwargs)
