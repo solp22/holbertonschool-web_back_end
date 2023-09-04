a411e5fc381567e3d81c760786e65240fd71c90f
solp22 <sol.puentereal@gmail.com>
2023-09-04 15:33:57 -0700

    fix

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -4,4 +4,5 @@
 
 def insert_school(mongo_collection, **kwargs):
     """ inserts a new document in a collection"""
-    return mongo_collection.insert_one(kwargs)
+    output = mongo_collection.insert_one(kwargs)
+    return output.inserted_id
