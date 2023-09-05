f7cc2f7966bdf05ac24007a024c5d5f0fff697af
solp22 <sol.puentereal@gmail.com>
2023-09-05 13:44:15 -0700

    typo

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -6,7 +6,7 @@
 def schools_by_topic(mongo_collection, topic):
     """returns the list of schools having a specific topic"""
     schools_list = []
-    query =  mongo_collection.find({"topic": topic})
+    query =  mongo_collection.find({"topics": topic})
     for school in query:
         schools_list.append(school)
     return schools_list
