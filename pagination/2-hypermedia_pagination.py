a3a3f92bba5d5553277172e104aa117abfbf29e2
solp22 <sol.puentereal@gmail.com>
2023-09-06 13:35:07 -0700

    change assert

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -33,7 +33,8 @@ class Server:
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """return the appropriate page of the dataset """
-        assert type(page) == int and type(page_size) == int
+        assert type(page) == int
+        assert type(page_size) == int
         assert page > 0 and page_size > 0
         range = index_range(page, page_size)
         data = self.dataset()
