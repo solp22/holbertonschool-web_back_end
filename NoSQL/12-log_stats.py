7edee7f90ffa59b586e13797d7dfff3f450520b9
solp22 <sol.puentereal@gmail.com>
2023-09-06 07:22:24 -0700

    fix mongo client

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 
 
 if __name__ == "__main__":
-    client = MongoClient(mongodb://127.0.0.1:27017)
+    client = MongoClient("mongodb://127.0.0.1:27017")
     db = client.logs
     collection = db.nginx
     gets = collection.find({"method": "GET"}).count()
