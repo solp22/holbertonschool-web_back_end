4f5b1c4b0b4021b1550603dd9acfa0d31e3d2d24
solp22 <sol.puentereal@gmail.com>
2023-09-06 07:16:46 -0700

    fix main

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -3,23 +3,23 @@
 from pymongo import MongoClient
 
 
-client = MongoClient()
-db = client.logs
-collection = db.nginx
-gets = collection.find({"method": "GET"}).count()
-posts = collection.find({"method": "POST"}).count()
-puts = collection.find({"method": "PUT"}).count()
-patches = collection.find({"method": "PATCH"}).count()
-deletes = collection.find({"method": "DELETE"}).count()
-status = collection.find({"method": "GET"}, 
-                         {"path": "/status"}).count()
+if __name__ == "__main__":
+    client = MongoClient()
+    db = client.logs
+    collection = db.nginx
+    gets = collection.find({"method": "GET"}).count()
+    posts = collection.find({"method": "POST"}).count()
+    puts = collection.find({"method": "PUT"}).count()
+    patches = collection.find({"method": "PATCH"}).count()
+    deletes = collection.find({"method": "DELETE"}).count()
+    status = collection.find({"method": "GET"}, 
+                            {"path": "/status"}).count()
 
-
-print (f"{collection.find().count()} logs")
-print ("Methods:")
-print (f"\tmethod GET: {gets}")
-print (f"\tmethod POST: {posts}")
-print (f"\tmethod PUT: {puts}")
-print (f"\tmethod PATCH: {patches}")
-print (f"\tmethod DELETE: {deletes}")
-print (f"{status} status check")
+    print (f"{collection.find().count()} logs")
+    print ("Methods:")
+    print (f"\tmethod GET: {gets}")
+    print (f"\tmethod POST: {posts}")
+    print (f"\tmethod PUT: {puts}")
+    print (f"\tmethod PATCH: {patches}")
+    print (f"\tmethod DELETE: {deletes}")
+    print (f"{status} status check")
