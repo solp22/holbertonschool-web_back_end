7940fed659341045410a6ea2042cbe2e691e2e14
solp22 <sol.puentereal@gmail.com>
2023-09-06 12:49:33 -0700

    fix pycode and documentation

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -11,6 +11,7 @@ def index_range(page, page_size):
     first_index = last_index - page_size
     return (first_index, last_index)
 
+
 class Server:
     """Server class to paginate a database of popular baby names.
     """
@@ -31,6 +32,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """return the appropriate page of the dataset """
         assert isinstance(page, int) and isinstance(page_size, int)
         assert page > 0 and page_size > 0
         range = index_range(page, page_size)
