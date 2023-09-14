893c5dd17f269fcca33cdf7f2847423c69eac0f9
solp22 <sol.puentereal@gmail.com>
2023-09-14 10:46:06 -0700

    fix

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,6 +1,6 @@
 export default function createReportObject(employeesList) {
   const reportObject = {
-    [allEmployees]: employeesList,
+    ["allEmployees"]: employeesList,
     getNumberOfDepartments(employeesList) {
       return Object.keys(employeesList).length;
     },
