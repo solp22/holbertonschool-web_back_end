ecac6f21c4d76ac559710bf1cafc8fbe589fc6dc
solp22 <sol.puentereal@gmail.com>
2023-09-14 14:31:55 -0700

    task 3

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,38 @@
+export default class Currency {
+  constructor(name, code) {
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be string');
+    }
+    if (typeof code !== 'string') {
+      throw TypeError('Code must be a number');
+    }
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be string');
+    }
+    this._name = name;
+  }
+
+  set code(code) {
+    if (typeof code !== 'string') {
+      throw TypeError('Code must be string');
+    }
+    this._code = code;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`
+  }
+}
