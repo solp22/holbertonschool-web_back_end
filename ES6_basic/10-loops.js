e668f0c75e0306a7099eac304cda96c15b761884
solp22 <sol.puentereal@gmail.com>
2023-09-14 10:08:08 -0700

    fix lint

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-    let newArray = [];
-    for (let idx of array) {
-      newArray.push(appendString + idx)
-    }
-  
-    return newArray;
+  const newArray = [];
+  for (const idx of array) {
+    newArray.push(appendString + idx);
   }
+
+  return newArray;
+}
