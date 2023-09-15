9005466541e12f2cec74c42e8848697a7bd965a5
solp22 <sol.puentereal@gmail.com>
2023-09-15 09:49:27 -0700

    lint

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,6 +1,4 @@
-import Currency from './3-currency';
-
-export default class Pricing{
+export default class Pricing {
   constructor(amount, currency) {
     this._amount = amount;
     this._currency = currency;
