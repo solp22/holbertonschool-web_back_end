19df72b37a502c7fc08af86dc6ba8a9f3892f530
root <sol.puentereal@gmai.com>
2023-10-02 11:00:02 -0700

    task 2

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,14 @@
+export default function handleResponseFromAPI(promise) {
+  return new Promise(((myResolve, myReject) => {
+    myResolve({
+      status: 200,
+      body: 'success',
+    });
+    if (myReject) {
+      Error();
+    }
+    promise.then(
+      console.log('Got a response from the API'),
+    );
+  }));
+}
