15905b05ca1c6ec03d82e5d9efe61381e122fc82
root <sol.puentereal@gmai.com>
2023-10-02 10:45:56 -0700

    fix if statement

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,12 +1,12 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise(((myResolve, myReject) => {
-    if (success === 'true') {
+    if (success) {
       myResolve({
         status: 200,
         body: 'Success',
       });
     } else {
-      myReject(new Error('The fake API is not working currently'));
+      myReject(('The fake API is not working currently'));
     }
   }));
 }
