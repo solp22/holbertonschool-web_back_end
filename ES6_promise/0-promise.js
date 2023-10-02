6af9a27d0da8059a3adc2a00b4762b6ed46871c9
root <sol.puentereal@gmai.com>
2023-10-02 09:20:14 -0700

    lint

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,8 +1,8 @@
 export default function getResponseFromAPI() {
-    return new Promise(function(myResolve, myReject) {
-        myResolve({ message: 'OK' })
-        if (myReject) {
-            throw new Error('Error');
-        }
-    });
+  return new Promise(((myResolve, myReject) => {
+    myResolve({ message: 'OK' });
+    if (myReject) {
+      throw new Error('Error');
+    }
+  }));
 }
